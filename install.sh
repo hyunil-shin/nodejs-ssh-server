18ac0e78822311c786f42a462f45bcfaaafe98f8
신현일/클라우드개발센터/NE <hyunil.shin@nhnent.com>
2020-01-16 09:53:15 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 curl https://raw.githubusercontent.com/hyunil-shin/nodejs-ssh-server/master/ssh-server.js -o ssh-server.js
 npm install fs crypto util shelljs ssh2 --save
-ssh-keygen -t rsa -b 2048 -f ./host.key
+ssh-keygen -t rsa -b 2048 -f ./host.key -q -N ""
